30ba3b0904f6087df1d6e0df1c8595f2106107d4
Кванториум <@intsrv.vnschool37.ru>
2024-12-24 18:06:13 +0300

    ff

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ secret_key = os.urandom(32)
 
 def create_app():
     app = Flask(__name__)
-
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = secret_key
@@ -32,26 +31,25 @@ def create_user_and_education():
         name = request.form['name']
         age = request.form['age']
         city = request.form['city']
-
         pic = request.files["pic"]
         filename = secure_filename(pic.filename)
         mimetype = pic.mimetype
-
         user = User(name=name, age=age, city=city, pic=pic.read(), filename=filename, mimetype=mimetype)
-
         education = Education(institution=request.form['education_institution'],
                               graduation_year=request.form['graduation_year'],
                               link=request.form['link'],
                               user=user)
-
-
+        contact = Contact(phone=request.form["phone"],
+                          tg=request.form["tg"],
+                          user=user)
         user.education.append(education)
+        user.contact.append(contact)
         try:
             db.session.add(user)
             db.session.commit()
             return redirect("/")
-        except:
-            return 'При добавлении поста произошла ошибка'
+        except Exception as error:
+            return f'При добавлении поста произошла ошибка: {error}'
     else:
         return render_template("create.html")
 
@@ -65,17 +63,21 @@ def get_img(id):
     return Response(pic.pic, mimetype=pic.mimetype)
 
 @app.route("/<int:id>")
-def posts_detail(id):
+def profile(id):
     user = User.query.get(id)
+    users = User.query.all()
+    contact = user.contact.all() if user else None
     educations = user.education.all() if user else None
-    return render_template('index.html', user=user, educations=educations)
+    return render_template('index.html', users=users, user=user, educations=educations, contact=contact)
 
 
 @app.route("/")
 def index():
     user = User.query.first()
+    users = User.query.all()
     educations = user.education.all() if user else None
-    return render_template('index.html', user=user, educations=educations)
+    contact = user.contact.all() if user else None
+    return render_template('index.html', users=users, user=user, educations=educations, contact=contact)
 
 
 if __name__ == "__main__":
